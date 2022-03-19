cb85814ae0fd1bccb422d57cfe26bc59d1479a5f
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2022-03-19 12:07:41 +0530

    Update Do-you-mean.ts

--- src/commands/General/Do-you-mean.ts
+++ src/commands/General/Do-you-mean.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://c.tenor.com/3mGN80gIcfUAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4'
+            'https://c.tenor.com/Mqc349Zmh94AAAPo/wataten-hana.mp4'
         ]
         let chitoge = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
